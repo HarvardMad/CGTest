61e301b17eb5711b44540648d7ada337fa826c22
Lalin <laleendra@gmail.com>
2018-09-29 10:03:04 +0100

    fixed maven to run tests with the build

--- src/com/cg/shop/CapGeminiCart.java
+++ src/com/cg/shop/CapGeminiCart.java
@@ -8,103 +8,173 @@ import java.util.List;
  */
 public class CapGeminiCart {
 
-    private List<String> itemsList =null;
+  private List<String> itemsList = null;
 
-    Fruit_Enum appleCost = Fruit_Enum.APPLE;
-    Fruit_Enum orangeCost = Fruit_Enum.ORANGE;
-
-    BigDecimal appleUnitCost = appleCost.getFruitPrice().setScale(2,BigDecimal.ROUND_CEILING);
-    BigDecimal orangeUnitCost = orangeCost.getFruitPrice().setScale(2,BigDecimal.ROUND_CEILING);
+  Fruit_Enum appleCost = Fruit_Enum.APPLE;
+  Fruit_Enum orangeCost = Fruit_Enum.ORANGE;
+  Fruit_Enum bananaCost = Fruit_Enum.BANANA;
 
 
-    private long totalAppleCount =0;
-
-    private long totalOrangeCount =0;
-
-
-    public CapGeminiCart(List cartItems){
-        this.itemsList = cartItems;
-        this.totalAppleCount =itemsList
-                .stream()
-                .filter(fruit->"apple".equalsIgnoreCase(fruit))
-                .count();
-
-        this.totalOrangeCount =itemsList
-                .stream()
-                .filter(fruit->"orange".equalsIgnoreCase(fruit))
-                .count();
-
-    }
-
-    public void setTotalOrangeCount(long totalOrangeCount) {
-        this.totalOrangeCount = totalOrangeCount;
-    }
+  BigDecimal appleUnitCost = appleCost.getFruitPrice().setScale(2, BigDecimal.ROUND_CEILING);
+  BigDecimal orangeUnitCost = orangeCost.getFruitPrice().setScale(2, BigDecimal.ROUND_CEILING);
+  BigDecimal bananaUnitCost = bananaCost.getFruitPrice().setScale(2, BigDecimal.ROUND_CEILING);
 
 
-    public long getTotalAppleCount() {
-        return totalAppleCount;
-    }
+  private long totalAppleCount = 0;
 
-    public void setTotalAppleCount(long totalAppleCount) {
-        this.totalAppleCount = totalAppleCount;
-    }
+  private long totalOrangeCount = 0;
 
-    public long getTotalOrangeCount() {
-        return totalOrangeCount;
-    }
+  private long totalBananaCount = 0;
 
-    public BigDecimal getCartTotal(){
-        return getTotalCostOfApples().add(getTotalCostOfOranges());
-    }
 
-    public BigDecimal getTotalCostOfApples(){
+  public CapGeminiCart(List cartItems) {
+    this.itemsList = cartItems;
+    this.totalAppleCount = itemsList
+        .stream()
+        .filter(fruit -> "apple".equalsIgnoreCase(fruit))
+        .count();
 
-        applyAppleOffer();
+    this.totalOrangeCount = itemsList
+        .stream()
+        .filter(fruit -> "orange".equalsIgnoreCase(fruit))
+        .count();
 
-        return appleUnitCost.multiply(BigDecimal.valueOf(getTotalAppleCount())).setScale(2,BigDecimal.ROUND_CEILING);
-    }
+    this.totalBananaCount = itemsList
+        .stream()
+        .filter(fruit -> "banana".equalsIgnoreCase(fruit))
+        .count();
 
-    public BigDecimal getTotalCostOfOranges(){
+  }
 
-        applyOrangeOffer();
+  public long getTotalBananaCount() {
+    return totalBananaCount;
+  }
 
-        return orangeUnitCost.multiply(BigDecimal.valueOf(getTotalOrangeCount())).setScale(2,BigDecimal.ROUND_CEILING);
-    }
+  public void setTotalBananaCount(long totalBananaCount) {
+    this.totalBananaCount = totalBananaCount;
+  }
 
-    public void applyAppleOffer(){
-        long appleCounter =0;
-        long reminaingApplesAfterDiscount =0;
+  public void setTotalOrangeCount(long totalOrangeCount) {
+    this.totalOrangeCount = totalOrangeCount;
+  }
 
-        if(getTotalAppleCount()%2==0){
-            setTotalAppleCount(getTotalAppleCount()/2);
+
+  public long getTotalAppleCount() {
+    return totalAppleCount;
+  }
+
+  public void setTotalAppleCount(long totalAppleCount) {
+    this.totalAppleCount = totalAppleCount;
+  }
+
+  public long getTotalOrangeCount() {
+    return totalOrangeCount;
+  }
+
+  public BigDecimal getCartTotal() {
+
+    return getTotalCostOfApples().add(getTotalCostOfOranges()).add(getTotalCostOfBananas());
+  }
+
+  public BigDecimal getTotalCostOfApples() {
+
+    applyAppleOffer();
+
+    return appleUnitCost.multiply(BigDecimal.valueOf(getTotalAppleCount())).setScale(2, BigDecimal.ROUND_CEILING);
+  }
+
+  public BigDecimal getTotalCostOfOranges() {
+
+    applyOrangeOffer();
+
+    return orangeUnitCost.multiply(BigDecimal.valueOf(getTotalOrangeCount())).setScale(2, BigDecimal.ROUND_CEILING);
+  }
+
+  public BigDecimal getTotalCostOfBananas() {
+
+    applyBananaOffer();
+
+    return bananaUnitCost.multiply(BigDecimal.valueOf(getTotalBananaCount())).setScale(2, BigDecimal.ROUND_CEILING);
+  }
+
+  public void applyAppleOffer() {
+    long appleCounter = 0;
+    long reminaingApplesAfterDiscount = 0;
+
+    if (getTotalAppleCount() % 2 == 0) {
+      setTotalAppleCount(getTotalAppleCount() / 2);
+    } else {
+      for (int i = 1; i <= getTotalAppleCount(); i++) {
+        if (i % 2 == 0) {
+          appleCounter++;
         }
-        else {
-            for (int i = 1; i <= getTotalAppleCount(); i++) {
-                if (i % 2 == 0) {
-                    appleCounter++;
-                }
-            }
-        }
-        reminaingApplesAfterDiscount = getTotalAppleCount()%2;
-        setTotalAppleCount(appleCounter + reminaingApplesAfterDiscount);
+      }
     }
+    reminaingApplesAfterDiscount = getTotalAppleCount() % 2;
+    setTotalAppleCount(appleCounter + reminaingApplesAfterDiscount);
+  }
 
-    public void applyOrangeOffer(){
-        long orangeCounter =0;
-        long remainingOrangesAfterDiscount = 0;
-        if(getTotalOrangeCount()%3==0){
-            setTotalOrangeCount(getTotalOrangeCount()*2/3);
+  public void applyOrangeOffer() {
+    long orangeCounter = 0;
+    long remainingOrangesAfterDiscount = 0;
+    if (getTotalOrangeCount() % 3 == 0) {
+      setTotalOrangeCount(getTotalOrangeCount() * 2 / 3);
+    } else {
+      for (int i = 1; i <= getTotalOrangeCount(); i++) {
+        if (i % 3 == 0) {
+          orangeCounter += 2;
         }
-        else {
-            for (int i = 1; i <= getTotalOrangeCount(); i++) {
-                if (i % 3 == 0) {
-                    orangeCounter += 2;
-                }
-            }
-        }
-        remainingOrangesAfterDiscount = getTotalOrangeCount()%3 ;
-
-        setTotalOrangeCount(orangeCounter + remainingOrangesAfterDiscount);
+      }
     }
+    remainingOrangesAfterDiscount = getTotalOrangeCount() % 3;
+
+    setTotalOrangeCount(orangeCounter + remainingOrangesAfterDiscount);
+  }
+
+  //happy path babanans only
+  public void applyBananaOffer() {
+    long bananaCounter = 0;
+    long remainingBananasAfterDiscount = 0;
+    if (getTotalOrangeCount() % 3 == 0) {
+      setTotalOrangeCount(getTotalOrangeCount() * 2 / 3);
+    } else {
+      for (int i = 1; i <= getTotalOrangeCount(); i++) {
+        if (i % 3 == 0) {
+          bananaCounter += 2;
+
+        }
+      }
+    }
+    remainingBananasAfterDiscount = getTotalBananaCount() % 3;
+
+    setTotalBananaCount(remainingBananasAfterDiscount + remainingBananasAfterDiscount);
+  }
+
+  public void applyOrangeBananaDiscount() {
+    // three for two on bananas and oranges cheapest item free
+    //4 b and 3 o
+    long bananaCounter = 0;
+    long orangeCounter =0;
+
+    long bananaCount = getTotalBananaCount();
+    long orangeCount = getTotalOrangeCount();
+
+    long totalOrangesAndBananas = bananaCount + orangeCount;
+
+    if (orangeCount == 0) {
+      applyBananaOffer();
+    } else if (bananaCount == 0) {
+      applyOrangeOffer();
+    } else {
+      for (long i = 1; i <= bananaCount; i++) {
+        if (i % 3 == 0) {
+          bananaCounter +=2;
+        }
+      }
+      long totalBananasAndOrangesForFullPay = bananaCount + orangeCount;
+
+
+    }
+  }
 
 }
--- src/com/cg/shop/Fruit_Enum.java
+++ src/com/cg/shop/Fruit_Enum.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
  * Created by LalinPethiyagoda on 10/09/2018.
  */
 public enum Fruit_Enum {
-     APPLE(0.25) , ORANGE (0.60) ;
+     APPLE(0.25) , ORANGE (0.60) , BANANA(0.70);
 
     private final double fruitPrice;
 
